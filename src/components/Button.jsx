4a9783fd0d4df0edca28534a60d5ce26795183c9
MandSolo <mandcashin@hotmail.co.uk>
2019-01-28 00:49:33 +0000

    added in rest of rows to app.js

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./Button.css";
 
 const isOperator = value => {
-    //checks to make sure the value inputted is a number!!!
+  //checks to make sure the value inputted is a number or . or =
   return !isNaN(value) || value === "." || value === "=";
 };
 
 const Button = props => (
+  //it returns the value if it is a number or . or = else it returns operator
   <div
     className={`button-wrapper ${
       isOperator(props.children) ? null : "operator"
