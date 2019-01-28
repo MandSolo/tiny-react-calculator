feb71b0b6732024b7dc56056ef8b5d32ba972008
MandSolo <mandcashin@hotmail.co.uk>
2019-01-28 00:57:51 +0000

    input button js and css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 class App extends Component {
+state= {
+  input: ""
+}
+
   render() {
     return (
       <div className="App">
         <div className="calc-wrapper">
+<Input input={this.state.input}></Input>
           <div className="row">
             <Button>7</Button>
             <Button>8</Button>
